058a21873f13901523e69e7e2ec11ae30310424a
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2026-08-12 13:05:55 +0330

    feat: add settings for enabled plugins and update AppFooter layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,13 +7,7 @@ const store = useWeddingStore()
 </script>
 
 <template>
-  <footer class="relative pt-16 pb-10 px-6 text-center" aria-label="Wedding footer">
-    <div class="flex items-center justify-center gap-3 text-rose-300">
-      <span class="h-px w-10 bg-gradient-to-r from-transparent to-rose-200" />
-      <FloralAccent class="text-xl" />
-      <span class="h-px w-10 bg-gradient-to-l from-transparent to-rose-200" />
-    </div>
-
+  <footer class="relative py-10 px-6 text-center" aria-label="Wedding footer">
     <p class="mt-6 font-display text-[2rem] text-rose-500 leading-none">
       {{ store.couple.partnerOne }}
       <Ampersand class="text-champagne-400 mx-1" />
